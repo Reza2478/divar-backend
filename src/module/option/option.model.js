42249b50009dfccf0e0d43a25861a5bf37497394
Reza <raminizade@gmail.com>
2024-11-04 13:47:25 +0330

    add required to option model and it's swagger

--- src/module/option/option.model.js
+++ src/module/option/option.model.js
@@ -2,12 +2,13 @@ const {Schema, Types, model} = require('mongoose');
 
 
 const OptionSchema = new Schema({
-    category: {type: Types.ObjectId, required: true, ref:'Category'},
+    category: {type: Types.ObjectId, required: true, ref: 'Category'},
     title: {type: String, required: true},
     key: {type: String, required: true},
     type: {type: String, enum: ["number", "string", "array", "boolean"]},
     enum: {type: Array, required: false},
     guid: {type: String, required: false},
+    required: {type: Boolean, required: false, default: false},
 })
 
 const OptionModel = model("Option", OptionSchema);
